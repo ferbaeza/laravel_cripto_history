b4496ac83cdc32c723dd2afb3a66cb421dead74f
Fer Baeza <fbaezahurtado@gmail.com>
2024-07-24 21:39:18 +0000

    feat(CRIPT-3)::Migrations

---
+++ src/Panel/Cripto/Application/PanelCriptoListarMonedasQuery.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Src\Panel\Cripto\Application;
+
+use Baezeta\Kernel\Query\Base\QueryBase;
+
+class PanelCriptoListarMonedasQuery extends QueryBase
+{
+    public function __construct(
+        
+    )
+        {
+            parent::__construct();
+    }
+
+}
---
+++ src/Panel/Cripto/Application/PanelCriptoListarMonedasQueryHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Src\Panel\Cripto\Application;
+
+class PanelCriptoListarMonedasQueryHandler
+{
+    public function __construct(
+        
+    )
+        {
+    }
+
+    public function run(PanelCriptoListarMonedasQuery $query)
+    {
+        dd($query);
+    }
+}
